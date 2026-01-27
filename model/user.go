effeb1a9c971d71f5061c425c3df93f15d281b88
Alexander Vega <kevinvega27100414@gmail.com>
2026-01-27 15:54:20 -0600

    Logro: Se actualizaron cambios para el recaptcha

--- model/user.go
+++ model/user.go
@@ -14,5 +14,5 @@ type UserForm struct {
 	Name           string `form:"name" validate:"required,min=2,max=50,no_special_chars"`
 	Email          string `form:"email" validate:"required,email"`
 	Phone          string `form:"phone" validate:"required,phone_format"`
-	RecaptchaToken string `form:"g-recaptcha-response" validate:"required"`
+	RecaptchaToken string `form:"recaptchaToken" validate:"required"` // ← CAMBIA ESTO
 }
